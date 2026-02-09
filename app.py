c6602d94d67b3b3e8be28098d7f85b712626ae80
Maria <mariaferoz1204@gmail.com>
2026-02-09 17:13:14 +0500

    Fixing SMTP timeout and adding env variables

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ db = SQLAlchemy(app)
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'apikey' 
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
